6c16314ffaec3f0158930832116c6b4268e18982
Howon Lee <howon@howonlee.com>
2014-10-18 01:01:22 -0700

    work done on bak net

--- net.py
+++ net.py
@@ -3,10 +3,13 @@ import numpy.random as npr
 
 class BakNet(object):
     def __init__(self):
-        self.in_layer = np.zeros(7, dtype=np.float)
-        self.out_layer = np.zeros(7, dtype=np.float)
-        self.h_weights = npr.random((100, 7), dtype=np.float)
-        self.o_weights = npr.random((7, 100), dtype=np.float)
+        self.n_in = 8
+        self.n_out = 8
+        self.n_hidden = 100
+        self.in_layer = np.zeros(n_in, dtype=np.float)
+        self.out_layer = np.zeros(n_out, dtype=np.float)
+        self.h_weights = npr.random((n_hidden, n_in), dtype=np.float)
+        self.o_weights = npr.random((n_out, n_hidden), dtype=np.float)
 
 if __name__ == "__main__":
     bnet = BakNet()

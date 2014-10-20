c36ba531f05c5ad179960977472f5eddfae15848
Howon Lee <howon@howonlee.com>
2014-10-19 19:04:57 -0700

    not actually getting the maximum I'm afraid

--- net.py
+++ net.py
@@ -4,22 +4,27 @@ import random
 
 class BakNet(object):
     def __init__(self):
-        self.delta = 0.05
-        self.in_l = 0
-        self.hid_l = 0
-        self.out_l = 1
-        self.hid_wgt = random.random() #in to hidden
-        self.out_wgt = random.random() #hidden to out
+        self.delta = 0.01
+        self.n_in = 2
+        self.n_hid = 3
+        self.n_out = 2
+        self.in_l = np.zeros(self.n_in)
+        self.hid_l = np.zeros(self.n_hid)
+        self.out_l = np.ones(self.n_out)
+        self.out_l[1] = 0
+        self.hid_wgt = npr.random((self.n_hid, self.n_in)) #3 rows 2 cols
+        self.out_wgt = npr.random((self.n_out, self.n_hid)) #2 rows 3 cols
 
     def fire(self):
-        curr_input = self.in_l #random input to activate
-        curr_hidden = self.hid_l #random hidden layer neuron to activate
-        curr_output = self.out_l #random output layer
+        curr_input = random.randint(0,self.in_l.shape[0]-1) #current input _index_
+        curr_hidden = np.argmax(self.hid_wgt, axis=0)[curr_input]
+        curr_output_idx = np.argmax(self.out_wgt, axis=0)[curr_hidden]
+        curr_output = self.out_l[curr_output_idx]
         if curr_output == curr_input:
             pass
         else:
-            self.out_wgt -= self.delta
-            self.hid_wgt -= self.delta
+            self.out_wgt[curr_output_idx, curr_hidden] -= self.delta
+            self.hid_wgt[curr_hidden, curr_input] -= self.delta
 
     def __str__(self):
         fmt_str = "delta: %s\nin layer: %s\nhidden layer: %s\nout layer: %s\nhidden weights: %s\nout weights: %s\n"

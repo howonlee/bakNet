f63ecc11c67c7c6670e2e911938cba5c33341e3f
Howon Lee <howon@howonlee.com>
2014-10-22 10:11:17 -0700

    multiple layers, woo

--- mnist.py
+++ mnist.py
@@ -3,12 +3,17 @@ import gzip
 import numpy as np
 from bak_net import BakNet
 
+def munge_pats(dataset):
+    pass
+
 if __name__ == "__main__":
     f = gzip.open("mnist.pkl.gz", "rb")
     train_set, valid_set, test_set = cPickle.load()
     #squish the training set and test set into the pattern formation
     f.close()
-    bnet = BakNet(784, 10000, 10, pats)
+    train_pats = munge_pats(train_set)
+    test_pats = munge_pats(test_set)
+    bnet = BakNet(784, 10000, 10, train_pats=train_pats, test_pats=test_pats)
     for i in xrange(500000):
         bnet.train()
     for i in xrange(5000):

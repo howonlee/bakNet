4bed616f3254d2479591c5c159017c1dbf7f7f84
Howon Lee <howon@howonlee.com>
2014-10-22 10:17:34 -0700

    move it again

--- 0_mnist.py
+++ 0_mnist.py
@@ -4,14 +4,23 @@ import numpy as np
 from bak_net import BakNet
 
 def munge_pats(dataset):
+    """
+    Instead of using the mnist dataset's formatting of the numpy things,
+    we munge towards python lists
+    why? we are lazy
+    """
+    data, labels = dataset
+    munged = []
+    for i in xrange(data.shape[0]):
+        munged.append(i,
     pass
 
 if __name__ == "__main__":
     f = gzip.open("mnist.pkl.gz", "rb")
-    train_set, valid_set, test_set = cPickle.load()
+    train_set, valid_set, test_set = cPickle.load(f)
     #squish the training set and test set into the pattern formation
     f.close()
-    print train_set
+    print train_set[0][4].shape, train_set[1][4].shape
     """
     train_pats = munge_pats(train_set)
     test_pats = munge_pats(test_set)

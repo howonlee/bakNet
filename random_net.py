14b1221569b0d87dfe914fed4ef4b26f30e86558
Howon Lee <howon@howonlee.com>
2014-10-27 12:05:46 -0700

    update random net

--- random_net.py
+++ random_net.py
@@ -1,10 +1,12 @@
 import numpy as np
 import scipy.sparse as sci_sp
+import lib_kron
 
 class RandomNet(object):
     def __init__(self):
-        self.architecture_matrix = sci_sp.dok_matrix((10000,10000))
-        #create the neural architecture, it should just be kronecker graph
+        kron_gen = lib_kron.create_generator(np.array([[0.5,0.5],[0.5,0.5]]), 6)
+        self.architecture_matrix = lib_kron.generate(kron_gen)
+        print self.architecture_matrix
 
         #pick a random set of 784 nodes with indegree 0, outdegree 1 to be in nodes
         #pick a random set of 1 node with indegree the most, outdegree 0 to be out node
@@ -20,4 +22,5 @@ class RandomNet(object):
         pass
 
 if __name__ == "__main__":
+    net = RandomNet()
     pass

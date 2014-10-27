d79328eac6018d1e8da3f62ce309182f156a86b0
Howon Lee <howon@howonlee.com>
2014-10-27 15:01:22 -0700

    more commit

---
+++ 1_mnist.py
@@ -0,0 +1,29 @@
+import cPickle
+import gzip
+import numpy as np
+import collections
+from random_net import RandomNet
+from datetime import datetime
+
+def munge_pats(dataset):
+    """
+    Instead of using the mnist dataset's formatting of the numpy things,
+    we munge towards python lists
+    why? we are lazy
+    """
+    data, labels = dataset
+    munged = collections.deque()
+    for i in xrange(data.shape[0]):
+        munged.append((data[i], labels[i]))
+    return munged
+
+if __name__ == "__main__":
+    """
+    Simple current benchmark as of 10/22/14: 1 layer of 10000 hidden units, 10001 training spikes, 5000 tests, get ~750 right answers in about 50sec - 0.85 error, pretty dismal
+    The theory is that there might be a critical state (a la critical states for ising models) for these problems. the model will stop at such a critical state and hopefully work really well
+    """
+    with gzip.open("mnist.pkl.gz", "rb") as f:
+        train_set, valid_set, test_set = cPickle.load(f)
+    train_pats = munge_pats(train_set)
+    test_pats = munge_pats(test_set)
+    net = RandomNet(train_pats=train_pats, test_pats=test_pats)
--- random_net.py
+++ random_net.py
@@ -1,12 +1,24 @@
 import numpy as np
+import numpy.random as npr
 import scipy.sparse as sci_sp
+import itertools
+import random
 import lib_kron
 
 class RandomNet(object):
-    def __init__(self):
-        kron_gen = lib_kron.create_generator(np.array([[0.5,0.5],[0.5,0.5]]), 6)
+    def __init__(self, train_pats, test_pats=None, exponent=10):
+        self.train_pats = train_pats
+        self.test_pats = train_pats if not test_pats else test_pats
+        self.exponent = exponent
+        kron_gen = lib_kron.create_generator(np.array([[0.5,0.5],[0.5,0.5]]), self.exponent)
         self.architecture_matrix = lib_kron.generate(kron_gen)
-        print self.architecture_matrix
+        for key in self.architecture_matrix.iterkeys():
+            self.architecture_matrix[key] = npr.random()
+        self.in_idxs = [x for x in xrange(784)]
+        self.out_idxs = [random.randint(785,2**self.exponent)]
+        self.nodes = npr.random(2**self.exponent)
+        print self.nodes
+
 
         #pick a random set of 784 nodes with indegree 0, outdegree 1 to be in nodes
         #pick a random set of 1 node with indegree the most, outdegree 0 to be out node
@@ -15,7 +27,6 @@ class RandomNet(object):
         #there should be a convolution option and a non-convolution option
         #weights are random
         #there should be no training
-        pass
 
     def test(self):
         #feed forward, mon ami

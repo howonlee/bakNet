304b927488f2448c6258d55542ef6bc3d8784224
Howon Lee <howon@howonlee.com>
2014-10-30 22:53:19 -0700

    add the sklearn stuff

--- hybrid_net.py
+++ hybrid_net.py
@@ -1,4 +1,10 @@
+import sys
 import numpy as np
+import numpy.random as npr
+from sklearn.datasets import load_digits
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.cross_validation import train_test_split
 
 def tanh_deriv(x):
     return 1.0 - x**2
@@ -16,11 +22,11 @@ class BakBPNet(object):
             self.activation_deriv = logistic_deriv
         elif activation == 'tanh':
             self.activation = np.tanh
-            self.activation_deriv = tanh_eriv
+            self.activation_deriv = tanh_deriv
         self.weights = []
-        for i in xrange(len(layers)):
-            self.weights.append((something))
-            self.weights.append((something else))
+        for i in range(1,len(layers)-1):
+            self.weights.append((2*np.random.random((layers[i-1] + 1, layers[i]+ 1))-1)*0.25)
+            self.weights.append((2*np.random.random((layers[i] + 1, layers[i+1]))-1)*0.25)
 
     def fit(self, X, y, learning_rate=0.2, epochs=10000, extremal=True):
         X = np.atleast_2d(X)
@@ -53,12 +59,34 @@ class BakBPNet(object):
         a = temp
         for l in range(0, len(self.weights)):
             a = self.activation(np.dot(a, self.weights[l]))
-            return a
+        return a
 
-if __name__ == "__main__":
+def xor_prob():
     nn = BakBPNet([2,2,1])
     X = np.array([[0,0],[0,1],[1,0],[1,1]])
-    y = np.aray([0,1,1,0])
+    y = np.array([0,1,1,0])
     nn.fit(X, y)
     for i in [[0,0], [0,1], [1,0], [1,1]]:
         print i, nn.predict(i)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "xor":
+        xor_prob()
+    else:
+        digits = load_digits() #from sklearn
+        X = digits.data
+        y = digits.target
+        X -= X.min()
+        X /= X.max()
+        nn = BakBPNet([64,100,10])
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+        labels_train = LabelBinarizer().fit_transform(y_train)
+        labels_test = LabelBinarizer().fit_transform(y_test)
+
+        nn.fit(X_train,labels_train,epochs=30000)
+        predictions = []
+        for i in range(X_test.shape[0]):
+            o = nn.predict(X_test[i] )
+            predictions.append(np.argmax(o))
+        print confusion_matrix(y_test,predictions)
+        print classification_report(y_test,predictions)

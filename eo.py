7cd8c3f5463203eb1f44c50317ac75a3870183e3
Howon Lee <howon@howonlee.com>
2014-11-26 14:41:25 -0800

    extremal optimization, round 2

---
+++ eo.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class EONet(object):
+    def __init__(self):
+        pass
+
+    def crossent(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+if __name__ == "__main__":
+    pass

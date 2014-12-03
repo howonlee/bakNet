616cde7387566b3fc533f5b75fea0a56365db4ee
Howon Lee <howon@howonlee.com>
2014-12-02 22:21:55 -0800

    cool beans

--- rbm/plot_error.py
+++ rbm/plot_error.py
@@ -4,12 +4,14 @@ import sys, re
 
 if __name__ == "__main__":
     non_decimal = re.compile(r'[^\d.]+')
-    with open("taueo_error", "r") as f:
+    with open("total_tau_eo_error", "r") as f:
         errs = map(lambda x: non_decimal.sub("", x), f.read().split())
         errs = map(float, errs)
+        errs = filter(lambda x: x > 100, errs)
         plt.plot(errs, "r.")
-    with open("opt_tau_eo_error", "r") as f:
+    with open("total_eo_error", "r") as f:
         errs = map(lambda x: non_decimal.sub("", x), f.read().split())
         errs = map(float, errs)
-        plt.plot(errs)
+        errs = filter(lambda x: x > 100, errs)
+        plt.plot(errs, "b-")
     plt.show()

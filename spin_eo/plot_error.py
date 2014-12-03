3b6523f6d8f0511158b6c3d206ad617c6f89ff00
Howon Lee <howon@howonlee.com>
2014-12-02 22:44:52 -0800

    zooming figures is great

--- spin_eo/plot_error.py
+++ spin_eo/plot_error.py
@@ -7,9 +7,13 @@ if __name__ == "__main__":
     with open("eo_all", "r") as f:
         errs = map(lambda x: non_decimal.sub("", x), f.read().split())
         errs = map(float, errs)
-        plt.plot(errs,"r-")
+        plt.plot(errs,"r-", label="EO")
     with open("tau_eo_all", "r") as f:
         errs = map(lambda x: non_decimal.sub("", x), f.read().split())
         errs = map(float, errs)
-        plt.plot(errs)
+        plt.plot(errs, label="tEO")
+    plt.title("Progression of Ising model energy")
+    plt.xlabel("epoch")
+    plt.ylabel("energy")
+    plt.legend()
     plt.show()

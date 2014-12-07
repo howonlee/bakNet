9f62bd81076ec99f7edd8c6f70fdc2439581d49e
Howon Lee <howon@howonlee.com>
2014-12-07 15:31:12 -0800

    something's wrong

--- bm/necker_eo.py
+++ bm/necker_eo.py
@@ -37,16 +37,31 @@ def update(activation, weights, tau=1.5):
     for i in xrange(16):
         for j in xrange(16):
             energies[i] += weights[i][j] * activation[j] * activation[i]
-    #print energies
-    unit = np.argmin(energies)
+    if tau < 0:
+        unit = np.argmin(energies)
+    else:
+        k = 16
+        while k > 15:
+            k = int(np.random.pareto(tau))
+        unit = energies.argsort()[-(k+1)]
     activation[unit] = 1 - activation[unit]
     return activation
 
 if __name__ == "__main__":
+    global_minima = set()
+    global_1 = np.array([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0])
+    global_2 = np.array([0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1])
+    global_1.flags.writeable = False
+    global_2.flags.writeable = False
+    global_minima.add(hash(global_1.data))
+    global_minima.add(hash(global_2.data))
     activations = [np.array(map(int, seq)) for seq in itertools.product("01", repeat=16)]
     for activation in activations:
         print "================"
         print activation
-        for i in xrange(100):
+        for i in xrange(1000):
             activation = update(activation, weights)
-            print activation
+            activation_copy = activation.copy()
+            activation_copy.flags.writeable = False
+            if hash(activation_copy.data) in global_minima:
+                print i

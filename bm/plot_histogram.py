01205ee60c3894b9a4e034834a5df67c04e25eb6
Howon Lee <howon@howonlee.com>
2014-12-07 16:48:36 -0800

    holy shit

--- bm/plot_histogram.py
+++ bm/plot_histogram.py
@@ -4,15 +4,23 @@ import collections
 import operator
 
 if __name__ == "__main__":
-    assert len(sys.argv) > 1
-    hist = collections.Counter()
-    with open(sys.argv[1], "r") as f:
+    hist1 = collections.Counter()
+    hist2 = collections.Counter()
+    with open("sa_res", "r") as f:
         for line in f:
             val = int(line)
-            hist[val] += 1
-    cts = hist.most_common()
-    print cts
-    plt.loglog(map(operator.itemgetter(1), cts), map(operator.itemgetter(0), cts), "ro")
+            hist1[val] += 1
+    with open("eo_res", "r") as f:
+        for line in f:
+            val = int(line)
+            hist2[val] += 1
+    cts1 = hist1.most_common()
+    cts2 = hist2.most_common()
+    print cts1
+    print "================"
+    print cts2
+    plt.loglog(map(operator.itemgetter(0), cts1), map(operator.itemgetter(1), cts1), "r.")
+    plt.loglog(map(operator.itemgetter(0), cts2), map(operator.itemgetter(1), cts2), "b.")
     plt.xlabel("number of iterations in trial")
     plt.ylabel("number of trials")
     plt.show()
